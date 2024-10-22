2bcd13446b68a47226f4b18fbff4460f849c8868
aliae2425 <noam.carmi80@gmail.com>
2024-10-22 20:21:21 +0200

    pagniation

--- skeleton/src/Controller/Admin/RecipeController.php
+++ skeleton/src/Controller/Admin/RecipeController.php
@@ -7,7 +7,6 @@ use App\Form\RecipeType;
 use App\Repository\RecetteRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -17,12 +16,19 @@ use Symfony\Component\Routing\Requirement\Requirement;
 class RecipeController extends AbstractController
 {
     #[Route(path:"/", name:"index")]
-    public function index(RecetteRepository $recetteRepository): Response
+    public function index(RecetteRepository $recetteRepository, Request $request): Response
     {
-        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        $this->denyAccessUnlessGranted('ROLE_USER');
         // dd($recetteRepository->findAll());
+        $page = $request->query->get('page', 1);
+        $LIMITE = 2;
+        $recette = $recetteRepository->paginateRecipies($page, $LIMITE);
+        $maxPage = ceil($recette->count() / $recette->getQuery()->getMaxResults());
         return $this->render("admin/recipe/index.html.twig", [
-            'recettes' =>  $recetteRepository->findAllWithCategory()
+            // 'recettes' =>  $recetteRepository->findAllWithCategory()
+            'recettes' =>  $recette, 
+            'maxPage' => $maxPage,
+            'page' => $page
         ]);
     }
 
--- skeleton/src/Repository/RecetteRepository.php
+++ skeleton/src/Repository/RecetteRepository.php
@@ -4,7 +4,9 @@ namespace App\Repository;
 
 use App\Entity\Recette;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @extends ServiceEntityRepository<Recette>
@@ -39,5 +41,17 @@ class RecetteRepository extends ServiceEntityRepository
             ->leftJoin("r.category", "c")
             ->getQuery()
             ->getResult();
+    }
+
+
+    public function paginateRecipies(int $page = 1, int $limite = 10): Paginator
+    {
+        return new Paginator($this
+            ->createQueryBuilder("r")
+            ->setMaxResults($limite)
+            ->setFirstResult(($page - 1 )* $limite)
+            ->getQuery()
+            ->setHint(Paginator::HINT_ENABLE_DISTINCT, false)
+        );
     }
 }
